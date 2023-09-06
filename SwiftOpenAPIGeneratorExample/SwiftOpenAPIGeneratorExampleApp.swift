43c368e6589ab54d9fa9064669ab77257ca6bb16
Mohamed Ramadan <mohamed.moslem50@gmail.com>
2023-09-06 14:09:11 +0300

    Initial Commit

---
+++ SwiftOpenAPIGeneratorExample/SwiftOpenAPIGeneratorExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftOpenAPIGeneratorExampleApp.swift
+//  SwiftOpenAPIGeneratorExample
+//
+//  Created by mohamed ramadan on 06/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftOpenAPIGeneratorExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
